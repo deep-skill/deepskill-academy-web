4aa28adaeada32d51fbb85e99f1b3195e37ff16d
Manuel Mandujano <manduuu2294@gmail.com>
2024-06-21 12:14:44 -0500

    the size and typeface of the programs component has been changed.

--- masterclass/src/components/BodyPythonEngineers.js
+++ masterclass/src/components/BodyPythonEngineers.js
@@ -29,35 +29,35 @@ export const Body = () => {
         <div className="bg-white p-4 lg:pb-0 w-full flex flex-col min-h-screen justify-center">
             <div className="md:pl-12 flex flex-col md:flex-row items-center md:items-start">
                 <div className="w-full md:w-1/2 mt-20 text-center md:text-left">
-                    <h1 className="text-customBlue font-timeburnerbold  text-5xl mt-14 leading-tight drop-shadow-lg md:text-8xl">
+                    <h1 className="text-customBlue font-timeburnerbold text-5xl mt-14 leading-tight drop-shadow-lg md:text-8xl">
                         Python<br />
-                        <span className="text-7xl md:text-80 font-fuentetitulo drop-shadow-lg">para ingenieros</span>
+                        <span className="text-40px md:text-80px font-fuentetitulo drop-shadow-lg">para ingenieros</span>
                     </h1>
-                    <p className="text-colorText mt-8 px-6 pl-1 text-2xl font-poppinsregular leading-normal md:text-xl sm:text-2xl">
+                    <p className="text-colorText mt-8 px-3 pl-1 text-xl font-poppinsregular leading-normal md:text-xl sm:text-2xl">
                         Programa en vivo, intensivo y ultra personalizado.
                     </p>
-                    <p className="text-colorText mt-6 px-6 pl-1 text-2xl font-poppinsregular leading-normal md:text-xl sm:text-2xl">
+                    <p className="text-colorText mt-6 px-3 pl-1 text-xl font-poppinsregular leading-normal md:text-xl sm:text-2xl">
                         Diseñado para ingenieros que buscan maximizar sus conocimientos sumando
                         la habilidad de tratar datos y automatizar procesos con programación.
                     </p>
                     <div className="flex flex-col mb-10 sm:flex-row items-center mt-12 space-y-6 md:mt-20 md:space-x-16 sm:space-y-0 sm:space-x-10">
                         <div className="flex flex-col items-center sm:flex-row sm:items-center">
                             <span className="icon-[mdi--calendar-range] text-colorsIcon w-9 h-9 mb-2 sm:mb-0 sm:mr-2"></span>
-                            <p className="text-colorText font-poppinsregular text-2xl  md:text-base ">8 semanas</p>
+                            <p className="text-colorText font-poppinsregular text-xl  md:text-base ">8 semanas</p>
                         </div>
                         <div className="flex flex-col items-center sm:flex-row sm:items-center">
                             <span className="icon-[material-symbols--online-prediction] text-colorsIcon w-9 h-9  mb-2 sm:mb-0 sm:mr-2"></span>
-                            <p className="text-colorText text-2xl sm:text-xl md:text-base font-poppinsregular">Virtual en vivo</p>
+                            <p className="text-colorText text-xl sm:text-xl md:text-base font-poppinsregular">Virtual en vivo</p>
                         </div>
                         <div className="flex flex-col items-center sm:flex-row sm:items-center">
                             <span className="icon-[mdi--account-group-outline] text-colorsIcon w-9 h-9 mb-2 sm:mb-0 sm:mr-2"></span>
-                            <p className="text-colorText text-2xl sm:text-xl md:text-base font-poppinsregular">Máximo 10 participantes</p>
+                            <p className="text-colorText text-xl sm:text-xl md:text-base font-poppinsregular">Máximo 10 participantes</p>
                         </div>
                     </div>
                 </div>
                 <div className="w-full md:w-1/2 flex flex-col items-center lg:mt-20 md:mt-0">
                     <div className="md:w-[50%] flex flex-col items-center bg-colorBackgroundCards" style={{ maxWidth: opts.width }}>
-                        <div className="bg-blue-500 font-poppinsbold font-extrabold lg:text-2xl text-white text-center w-full py-4 mt-0"> {/* Adjusted mt-4 */}
+                        <div className="bg-blue-500 font-poppinsbold font-extrabold  text-xl lg:text-2xl text-white text-center w-full py-4 mt-0"> {/* Adjusted mt-4 */}
                             ¡QUEDAN POCOS CUPOS!
                         </div>
                         <div className="mt-0">
--- masterclass/src/components/Programs.js
+++ masterclass/src/components/Programs.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 const Programs = () => {
   return (
-    <div className="w-full md:py-10 bg-colorBackground">
+    <div className="w-full md:py-10 bg-white">
       <div className="container mx-auto px-4 lg:px-0">
         <div className="px-2 lg:px-16 py-8 md:p-12">
-          <h2 className="text-3xl lg:text-4xl text-center font-bold font-poppins text-blue-700 mb-1 md:mb-1 md:text-left">
+          <h2 className="text-3xl lg:text-40px text-center font-poppinsbold text-blue-700 mb-1 md:mb-1 md:text-left">
             ¿Qué Incluye el Programa?
           </h2>
-          <p className="text-base text-center text-colorTextNavbar font-poppins font-bold mb-12 md:text-lg md:text-left lg:text-2xl">
+          <p className="text-base text-center text-colorTextNavbar  font-poppinssemibold mb-12 md:text-lg md:text-left lg:text-2xl">
             Beneficios para nuestra comunidad
           </p>
           <div className=" md:w-full flex flex-wrap justify-center">
@@ -17,7 +17,7 @@ const Programs = () => {
               <div className="p-1 bg-blue-500 text-white rounded-lg lg:p-8 h-full flex flex-col justify-between">
                 <div className="flex items-center mb-2 md:mb-4 lg:mb-0">
                   <img src="/images/icon/clock-icon.svg" alt="Clock Icon" className="w-36 h-36 mb-2 mr-3 md:w-25 md:h-25 lg:w-40 lg:h-40  sm:mb-0" />
-                  <p className="text-base md:text-2xl font-bold font-poppins md:pr-20">
+                  <p className="text-base md:text-3xl font-poppinsbold md:pr-20">
                     32 horas de masterclass en vivo con Deep Mentor.
                   </p>
                 </div>
@@ -28,7 +28,7 @@ const Programs = () => {
               <div className="p-1 bg-blue-500 text-white rounded-lg lg:p-8 h-full flex flex-col justify-between">
                 <div className="flex items-center mb-2 md:mb-4 lg:mb-0">
                   <img src="/images/icon/rocket-launch-icon.svg" alt="Rocket Launch Icon" className="w-36 h-36 mb-2 mr-3 md:w-25 md:h-25 lg:w-40 lg:h-40  sm:mb-0" />
-                  <p className="text-base md:text-2xl font-bold font-poppins md:pr-20">
+                  <p className="text-base md:text-3xl font-poppinsbold md:pr-20">
                     8 horas de trabajo de laboratorio 1 a 1 con Buddy.
                   </p>
                 </div>
@@ -39,7 +39,7 @@ const Programs = () => {
               <div className="p-4 lg:p-8 bg-colorsButton text-white rounded-lg h-full flex flex-col justify-between">
                 <div className="flex items-center mb-2 md:mb-4">
                   <img src="/images/icon/monitor-icon.svg" alt="Monitor Icon" className="w-28 h-28 mb-2 mr-2 md:w-15 md:h-15 lg:w-28 lg:h-28  sm:mb-0 sm:mr-2"/>
-                  <p className="text-base md:text-xl font-bold font-poppins text-black  md:text-left pl-5 md:pl-6">
+                  <p className="text-base md:text-xl font-poppinsbold text-black  md:text-left pl-5 md:pl-6">
                     Seguimiento asincrónico durante todo el programa.
                   </p>
                 </div>
@@ -50,7 +50,7 @@ const Programs = () => {
               <div className="p-6 lg:p-8 bg-colorsButton text-white rounded-lg h-full flex flex-col justify-between">
                 <div className="flex items-center mb-2 md:mb-4">
                   <img src="/images/icon/book-icon.svg" alt="Book Icon" className="w-28 h-28 mb-2 mr-2 md:w-15 md:h-15 lg:w-28 lg:h-28 sm:mb-0 sm:mr-2" />
-                  <p className="text-base md:text-xl font-bold font-poppins text-black  md:text-left pl-4 md:pl-6">
+                  <p className="text-base md:text-xl font-poppinsbold text-black  md:text-left pl-4 md:pl-6">
                     Acceso a las grabaciones y material de estudio.
                   </p>
                 </div>
@@ -61,7 +61,7 @@ const Programs = () => {
               <div className="p-6 lg:p-8 bg-colorsButton text-white rounded-lg h-full flex flex-col justify-between">
                 <div className="flex items-center mb-4">
                   <img src="/images/icon/book-icon2.svg" alt="Book Icon" className="w-28 h-28 md:w-15 md:h-15 lg:w-28 lg:h-28 mr-2 sm:mb-0 sm:mr-2" />
-                  <p className="text-base md:text-xl font-bold font-poppins text-black text-left pl-6">
+                  <p className="text-base md:text-xl font-poppinsbold text-black text-left pl-6">
                     Acceso a las grabaciones y material de estudio.
                   </p>
                 </div>
