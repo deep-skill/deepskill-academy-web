eafee5d580b9e0936fb37bdf2ee94770c1c6b71f
Manuel Mandujano <manduuu2294@gmail.com>
2024-06-22 00:29:10 -0500

    modified the size and typography of the coursecard component coursecard.

--- masterclass/src/components/CourseCard.js
+++ masterclass/src/components/CourseCard.js
@@ -7,22 +7,25 @@ import 'react-responsive-carousel/lib/styles/carousel.min.css';
 const CourseCard = () => {
     const courses = [
         {
-            title: 'PYTHON PARA INGENIEROS',
+            title: 'PYTHON FOR ENGINEERS',
             description: 'Programa en vivo, intensivo y ultra personalizado. Diseñado para ingenieros que buscan maximizar sus conocimientos sumando la habilidad de tratar datos y automatizar procesos con programación.',
             date: '00/00/0000',
             imageSrc: '/images/image-course.jpg',
+            link: '/python-for-engineers'
         },
         {
-            title: 'PYTHON PARA INGENIEROS',
+            title: 'BOOTCAMP PYTHON',
             description: 'Programa en vivo, intensivo y ultra personalizado. Diseñado para ingenieros que buscan maximizar sus conocimientos sumando la habilidad de tratar datos y automatizar procesos con programación.',
             date: '00/00/0000',
             imageSrc: '/images/image-course.jpg',
+            link: '/courses/bootcamp-python'
         },
         {
-            title: 'PYTHON PARA INGENIEROS',
+            title: 'MASTERCLASS ALGORITHMS',
             description: 'Programa en vivo, intensivo y ultra personalizado. Diseñado para ingenieros que buscan maximizar sus conocimientos sumando la habilidad de tratar datos y automatizar procesos con programación.',
             date: '00/00/0000',
             imageSrc: '/images/image-course.jpg',
+            link: '/courses/masterclass-algorithms'
         },
     ];
 
@@ -42,7 +45,7 @@ const CourseCard = () => {
     return (
         <div className="w-full bg-gray-900 text-white bg-cover bg-center pb-12 md:py-12" style={{ backgroundImage: 'url("/images/background-coursecard.jpg")' }}>
             <header className={`py-8 text-center md:text-left ${isMobile ? 'bg-blue-700 w-full' : ''}`}>
-                <h1 className={`font-poppins text-32px md:text-5xl lg:text-6xl md:ml-32 font-bold leading-relaxed ${isMobile ? 'p-2' : ''} drop-shadow-md`}>
+                <h1 className={`font-poppinsbold text-32px md:text-5xl lg:text-6xl md:ml-32 leading-relaxed ${isMobile ? 'p-2' : ''} drop-shadow-md`}>
                     Aprende a tu ritmo<br />
                     <span className={`${isMobile ? 'block text-2xl' : 'inline'} md:text-5xl lg:text-6xl`}>Profundiza tus habilidades</span>
                 </h1>
@@ -60,12 +63,12 @@ const CourseCard = () => {
                                             className="w-full h-48 object-cover rounded-t-lg"
                                         />
                                         <div className="p-6">
-                                            <h3 className="font-poppins text-left text-2xl font-bold mb-2">{course.title}</h3>
-                                            <p className="text-xl mb-4 text-justify font-poppins">{course.description}</p>
-                                            <p className="text-xl mb-6 font-poppins text-left"><strong>Próxima fecha de inicio: {course.date}</strong></p>
-                                            <div className="flex justify-center">
-                                                <Link href="/python-for-engineers">
-                                                    <button className="bg-colorsButton text-colorTextNavbar text-base font-poppins font-bold px-9 py-3 rounded-3xl">
+                                            <h3 className="font-poppinsbold text-left text-2xl mb-2">{course.title}</h3>
+                                            <p className="text-lg mb-6 text-justify font-poppinsregular">{course.description}</p>
+                                            <p className="text-xl mb-6 font-poppinssemibold text-left">Próxima fecha de inicio: {course.date}</p>
+                                            <div className="flex justify-center my-4">
+                                                <Link href={course.link}>
+                                                    <button className="bg-colorsButton text-colorTextNavbar text-base font-poppinssemibold px-9 py-3 rounded-3xl">
                                                         MÁS INFORMACIÓN AQUÍ
                                                     </button>
                                                 </Link>
@@ -84,12 +87,12 @@ const CourseCard = () => {
                                             className="w-full h-48 object-cover rounded-t-lg"
                                         />
                                         <div className="p-6">
-                                            <h3 className="text-xl font-semibold mb-2">{course.title}</h3>
-                                            <p className="text-sm mb-4 text-justify">{course.description}</p>
-                                            <p className="text-sm mb-6"><strong>Próxima fecha de inicio: {course.date}</strong></p>
-                                            <div className="flex justify-center">
-                                                <Link href="/python-for-engineers">
-                                                    <button className="bg-colorsButton text-colorTextNavbar text-sm font-semibold px-6 py-2 rounded-3xl">
+                                            <h3 className="text-xl font-poppinsbold mb-2">{course.title}</h3>
+                                            <p className="text-sm mt-5 mb-4 text-justify font-poppinsregular">{course.description}</p>
+                                            <p className="text-sm mb-6 font-poppinssemibold">Próxima fecha de inicio: {course.date}</p>
+                                            <div className="flex justify-center py-6">
+                                                <Link href={course.link}>
+                                                    <button className="bg-colorsButton text-colorTextNavbar text-sm font-poppinssemibold px-14 py-2 rounded-3xl">
                                                         MÁS INFORMACIÓN AQUÍ
                                                     </button>
                                                 </Link>
@@ -107,3 +110,4 @@ const CourseCard = () => {
 };
 
 export default CourseCard;
+
