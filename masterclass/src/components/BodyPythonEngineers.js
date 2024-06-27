138946c8368aaf387ea02a77b9e6f0467198ecd3
Manuel Mandujano <manduuu2294@gmail.com>
2024-06-26 19:55:07 -0500

    course name change

--- masterclass/src/components/BodyPythonEngineers.js
+++ masterclass/src/components/BodyPythonEngineers.js
@@ -31,9 +31,9 @@ export const Body = () => {
                 <div className="w-full md:w-1/2 mt-20 text-center md:text-left">
                     <h1 className="text-customBlue font-timeburnerbold text-5xl mt-14 leading-tight drop-shadow-lg md:text-8xl">
                         Python<br />
-                        <span className="text-40px md:text-8xl font-fuentetitulo leading-tight drop-shadow-lg">de Data</span>
+                        <span className="text-40px md:text-80px font-fuentetitulo leading-tight drop-shadow-lg">Fundamentals</span>
                     </h1>
-                    <p className="text-colorText mt-8 px-3 pl-1 text-xl font-poppinsregular leading-normal md:text-xl sm:text-2xl">
+                    <p className="text-colorText mt-8 px-3 pl-1 text-xl font-poppinsbold leading-normal md:text-xl sm:text-2xl">
                         Programa en vivo, intensivo y ultra personalizado.
                     </p>
                     <p className="text-colorText mt-6 px-3 pl-1 text-xl font-poppinsregular leading-normal md:text-xl sm:text-2xl">
