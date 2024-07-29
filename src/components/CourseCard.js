4d214ccba9749907e69ebf2abf4c6fe023016384
Manuel Mandujano <manduuu2294@gmail.com>
2024-07-28 22:18:03 -0500

    corrected text, and measurements of the main landings

--- src/components/CourseCard.js
+++ src/components/CourseCard.js
@@ -22,6 +22,7 @@ const CourseCard = () => {
         },
         {
             title: 'PYTHON y SQL PARA INGENIEROS',
+            subtitle: 'APLICADOS A PROYECTOS DE DATOS',
             description: [
                 'Entrenamiento de 12 semanas, en vivo y ultra personalizado.',
                 'Diseñado para profesionales de Ciencias e Ingenierías.',
@@ -51,9 +52,9 @@ const CourseCard = () => {
 
     return (
         <div className="w-full bg-gray-900 text-white bg-cover bg-center pb-12 md:pt-0 lg:pt-0 lg:pb-6 md:pb-6" style={{ backgroundImage: 'url("/images/background-coursecard.jpg")' }}>
-            <header id="my-courses" className={`lg:bg-blue-700 lg:w-[68%] 2xl:w-[65%] lg:rounded-br-[4rem] lg:py-10 2xl:py-8 py-7 text-center md:text-left ${isMobile ? 'bg-blue-700 w-full' : ''}`}>
-                <h1 className={`font-poppinsbold text-32px md:text-4xl lg:text-3xl xl:text-3xl 2xl:text-4xl md:ml-20 leading-relaxed ${isMobile ? 'p-2' : ''} drop-shadow-md`}>
-                    Aprende a tu ritmo <span className={`${isMobile ? 'block text-2xl' : 'inline'} md:text-5xl lg:text-3xl xl:text-3xl 2xl:text-4xl`}>profundiza tus habilidades</span>
+            <header id="my-courses" className={`lg:bg-blue-700 lg:w-[68%] xl:w-[55%] 2xl:w-[45%] lg:rounded-br-[4rem] lg:py-10 2xl:py-8 py-7 text-center md:text-left ${isMobile ? 'bg-blue-700 w-full' : ''}`}>
+                <h1 className={`font-poppinsbold text-4xl md:text-4xl lg:text-3xl xl:text-4xl 2xl:text-40px md:ml-20 leading-tight ${isMobile ? 'p-2' : ''} drop-shadow-md`}>
+                    Profundiza <span className={`${isMobile ? 'block text-32px' : 'inline'} md:text-5xl lg:text-3xl xl:text-4xl 2xl:text-4xl`}>tus habilidades</span>
                 </h1>
             </header>
             <div className="max-w-8xl mx-auto px-4 sm:px-4 2xl:px-8 xl:px-2 lg:px-1">
@@ -74,8 +75,9 @@ const CourseCard = () => {
                                             </h1>
                                         </header>
                                         <div className="p-4">
-                                            <h3 className="font-poppinsbold text-colorTextSubtitulo text-3xl mb-2 text-center">{course.title}</h3>
-                                            <ul className="font-poppinsregular px-2 text-gray-700 list-disc text-lg leading-normal text-left px-6">
+                                            <h3 className="font-poppinsbold text-colorTextSubtitulo text-3xl text-center">{course.title}</h3>
+                                            <h3 className="font-poppinsbold text-colorTextSubtitulo text-base mb-6 text-center">{course.subtitle}</h3>
+                                            <ul className="font-poppinsregular text-gray-700 list-disc text-lg leading-normal text-left px-6">
                                                 {course.description.map((item, idx) => (
                                                     <li key={idx}>{item}</li>
                                                 ))}
@@ -108,15 +110,16 @@ const CourseCard = () => {
                                         </header>
                                         <div className="px-12 lg:px-5 xl:px-8 flex flex-col flex-grow">
                                             <h3 className="text-colorTextSubtitulo text-xl text-center lg:text-3xl xl:text-3xl font-poppinsbold mb-2">{course.title}</h3>
-                                            <ul className="font-poppinsregular text-gray-700 list-disc text-xl lg:text-lg lg:px-6 leading-normal lg:leading-loose 2xl:leading-snug 2xl:text-base">
+                                            <h3 className="text-colorTextSubtitulo text-xl text-center lg:text-xl xl:text-xl 2xl:text-lg xl:-mt-2 lg:-mt-2 font-poppinsbold mb-2">{course.subtitle}</h3>
+                                            <ul className="font-poppinsregular text-gray-700 list-disc text-xl lg:text-lg lg:px-6 leading-normal lg:leading-loose xl:leading-snug 2xl:leading-snug 2xl:text-base">
                                                 {course.description.map((item, idx) => (
                                                     <li key={idx}>{item}</li>
                                                 ))}
                                             </ul>
-                                            <p className="2xl:text-lg xl:text-lg lg:text-xl my-5 font-poppinssemibold">Fecha de inicio: {course.date}</p>
+                                            <p className="2xl:text-lg xl:text-xl lg:text-xl my-5 font-poppinssemibold">Fecha de inicio: {course.date}</p>
                                             <div className="flex justify-center py-2 2xl:pb-2 mt-auto mb-6">
                                                 <Link href={course.link}>
-                                                    <button className="bg-colorsButton text-colorTextNavbar text-sm lg:text-base xl:text-base font-poppinssemibold px-14 py-3 rounded-3xl">
+                                                    <button className="bg-colorsButton text-colorTextNavbar text-sm lg:text-base xl:text-lg font-poppinssemibold px-14 py-3 rounded-3xl">
                                                         MÁS INFORMACIÓN AQUÍ
                                                     </button>
                                                 </Link>
