4af777e39ab2455c9f209d535f0d7c05c903cc17
Manuel Mandujano <manduuu2294@gmail.com>
2024-07-29 00:40:22 -0500

    corrected text, and landings measurement algorithms

--- src/components/Programs.js
+++ src/components/Programs.js
@@ -18,7 +18,7 @@ const Programs = () => {
                 <div className="flex items-center mb-2 md:mb-4 lg:mb-0">
                   <img src="/images/icon/clock-icon.svg" alt="Clock Icon" className="w-36 h-36 mb-2 mr-3 md:w-25 md:h-25 lg:w-40 lg:h-40  sm:mb-0" />
                   <p className="font-poppinsbold text-xl md:text-2xl lg:text-xl xl:text-2xl 2xl:text-2xl md:pr-20">
-                    16 horas de masterclass en vivo con Deep Mentor.
+                    24 horas de sesiones grupales en vivo con tu Deep Mentor.
                   </p>
                 </div>
               </div>
@@ -29,7 +29,7 @@ const Programs = () => {
                 <div className="flex items-center mb-2 md:mb-4 lg:mb-0">
                   <img src="/images/icon/rocket-launch-icon.svg" alt="Rocket Launch Icon" className="w-36 h-36 mb-2 mr-3 md:w-25 md:h-25 lg:w-40 lg:h-40  sm:mb-0" />
                   <p className="font-poppinsbold text-xl md:text-2xl lg:text-xl xl:text-2xl 2xl:text-2xl md:pr-20">
-                     4 horas de trabajo de laboratorio 1 a 1 con Buddy.
+                   4 horas de trabajo de laboratorio en sesión 1 a 1 con tu Buddy.
                   </p>
                 </div>
               </div>
@@ -39,7 +39,7 @@ const Programs = () => {
               <div className="p-4 lg:p-4 xl:py-6 2xl:py-8 2xl:p-7 bg-colorsButton text-white rounded-lg h-full flex flex-col justify-between">
                 <div className="flex items-center mb-2 md:mb-4">
                   <img src="/images/icon/monitor-icon.svg" alt="Monitor Icon" className="w-28 h-28 mb-2 mr-2 md:w-15 md:h-15 lg:w-28 lg:h-28  sm:mb-0 sm:mr-2"/>
-                  <p className="font-poppinsbold  text-black text-xl md:text-lg lg:text-base xl:text-base 2xl:text-xl md:text-left pl-5 md:pl-6">
+                  <p className="font-poppinsbold  text-black text-xl md:text-lg lg:text-sm xl:text-lg 2xl:text-xl md:text-left pl-5 md:pl-6">
                     Seguimiento asíncrono full time durante todo el programa
                   </p>
                 </div>
@@ -50,7 +50,7 @@ const Programs = () => {
               <div className="p-6 lg:p-4 xl:py-6 2xl:py-8 2xl:p-7 bg-colorsButton text-white rounded-lg h-full flex flex-col justify-between">
                 <div className="flex items-center mb-2 md:mb-4">
                   <img src="/images/icon/book-icon.svg" alt="Book Icon" className="w-28 h-28 mb-2 mr-2 md:w-15 md:h-15 lg:w-28 lg:h-28 sm:mb-0 sm:mr-2" />
-                  <p className="font-poppinsbold text-black text-xl md:text-lg lg:text-base xl:text-base 2xl:text-xl md:text-left pl-4 md:pl-6">
+                  <p className="font-poppinsbold text-black text-xl md:text-lg lg:text-sm xl:text-lg 2xl:text-xl md:text-left pl-4 md:pl-6">
                     Acceso premium a nuestra comunidad de Discord.
                   </p>
                 </div>
@@ -61,7 +61,7 @@ const Programs = () => {
               <div className="p-6 lg:p-4 xl:py-6 2xl:py-8 2xl:p-7 bg-colorsButton text-white rounded-lg h-full flex flex-col justify-between">
                 <div className="flex items-center mb-4">
                   <img src="/images/icon/book-icon2.svg" alt="Book Icon" className="w-28 h-28 md:w-15 md:h-15 lg:w-28 lg:h-28 mr-2 sm:mb-0 sm:mr-2" />
-                  <p className="font-poppinsbold text-black text-xl md:text-lg lg:text-base xl:text-base 2xl:text-xl  text-left pl-6">
+                  <p className="font-poppinsbold text-black text-xl md:text-lg lg:text-sm xl:text-lg 2xl:text-xl  text-left pl-6">
                     Acceso a todas las grabaciones y material de estudio.
                   </p>
                 </div>
--- src/components/course-algorithms-data/MasterClassContent.js
+++ src/components/course-algorithms-data/MasterClassContent.js
@@ -21,9 +21,9 @@ const MasterclassContent = () => {
     return (
         <div className="w-full flex flex-col min-h-screen">
             <div className="relative w-full h-full bg-black bg-opacity-75 py-10 lg:pb-2" style={{ backgroundImage: 'url("/images/background-masterclass.png")', backgroundBlendMode: 'overlay' }}>
-                <header className={`absolute top-0 left-0 lg:bg-customBlue lg:w-[50%] lg:rounded-br-[4rem] lg:py-6 py-5 text-center md:text-left ${isMobile ? 'bg-blue-700 w-full' : ''}`}>
-                    <h1 className={`font-poppinsbold lg:-mb-2 text-32px md:text-4xl lg:text-3xl xl:text-4xl 2xl:text-40px md:ml-20 lg:leading-relaxed ${isMobile ? 'p-2' : ''} drop-shadow-md`}>
-                        Contenido de la masterclass <span className={`${isMobile ? 'block text-2xl' : 'inline'} md:text-5xl lg:text-4xl`}></span>
+                <header className={`absolute top-0 left-0 lg:bg-customBlue lg:w-[50%] 2xl:w-[45%] lg:rounded-br-[4rem] lg:py-6 py-5 text-center md:text-left ${isMobile ? 'bg-blue-700 w-full' : ''}`}>
+                    <h1 className={`font-poppinsbold lg:-mb-2 leading-tight text-32px md:text-4xl lg:text-3xl xl:text-4xl 2xl:text-40px md:ml-20 lg:leading-relaxed ${isMobile ? 'p-2' : ''} drop-shadow-md`}>
+                        Contenido del bootcamp <span className={`${isMobile ? 'block text-2xl' : 'inline'} md:text-5xl lg:text-4xl`}></span>
                     </h1>
                     {!isMobile && (
                         <h3 className={`font-poppinssemibold text-xl md:text-3xl lg:text-xl xl:text-xl 2xl:text-xl md:ml-20 xl:leading-10 2xl:leading-loose ${isMobile ? 'p-0' : ''} drop-shadow-md`}>
@@ -31,21 +31,20 @@ const MasterclassContent = () => {
                         </h3>
                     )}
                 </header>
-                <div className="container px-8 sm:px-8 md:px-10 lg:px-1 xl:px-10 pt-40 2xl:pt-24">
-                    <div className="flex flex-wrap items-center mb-8 lg:space-x-24 xl:space-x-20">
+                <div className="container px-8 sm:px-8 md:px-10 lg:px-1 xl:px-10 pt-40 xl:pt-40 2xl:pt-24">
+                    <div className="flex flex-wrap items-center mb-8 lg:space-x-20 xl:space-x-20">
                         <div className="w-full md:w-1/2 lg:px-12 xl:px-12">
                             <p className="font-poppinsregular text-white lg:text-xl xl:text-xl 2xl:text-xl text-xl text-center lg:text-justify md:text-left mb-10 lg:mb-10 xl:mb-15 2xl:mb-10">
                                 Organizamos junto a nuestros expertos un temario que te ayudará a alcanzar tus metas profesionales.
                             </p>
-                            <button className="bg-customBlue font-poppinssemibold text-white lg:text-base text-base py-2 px-10 rounded-full block mb-12">
+                            <button className="bg-customBlue font-poppinssemibold  text-white lg:text-base text-base py-2 px-10 rounded-full block mb-12">
                                 <a href="https://drive.google.com/file/d/1ino9AR9Jz-FBvTgKglTN6yEVc5f7p0IZ/view?usp=sharing" target="_blank" rel="noopener noreferrer">
                                     TEMARIO DETALLADO
                                 </a>
                             </button>
                         </div>
-                        <div className="w-full lg:w-2/5 bg-customBlue p-8 px-10 lg:p-10 lg:px-10 xl:px-18 2xl:px-18 lg:py-14 rounded-[38px]">
-                            <h3 className="font-poppinsregular text-white text-xl lg:text-lg mb-2">Temario</h3>
-                            <h4 className="font-poppinsbold text-white text-xl lg:text-xl mb-2">Algorithms & Data Structures</h4>
+                        <div className="w-full lg:w-2/5 2xl:w-[35%] bg-customBlue p-8 px-10 lg:p-18 lg:px-10 xl:px-18 2xl:px-18 lg:py-14 rounded-[38px]">
+                            <h4 className="font-poppinsbold text-white text-xl lg:text-xl mb-2">Algoritmos</h4>
                             <ul className="font-poppinsregular text-white list-disc text-xl lg:text-lg lg:px-6 leading-normal lg:leading-loose">
                                 <li>Arrays, Strings and Hash Tables</li>
                                 <li>Linked List, Stack and Queues</li>
@@ -60,20 +59,20 @@ const MasterclassContent = () => {
                     </div>
                 </div>
                 <div className="w-full text-center text-white py-0 px-0">
-                    <p className="font-poppinssemibold lg:pt-2 xl:pt-2 2xl:pt-2 text-2xl lg:text-xl lg:text-left lg:ml-20">Tecnologías utilizadas</p>
+                    <p className="font-poppinssemibold lg:pt-0 xl:pt-4 2xl:pt-2 text-2xl lg:text-xl lg:text-left lg:ml-20">Tecnologías utilizadas</p>
                     <div className="container mx-auto px-4 sm:px-8 md:px-16 lg:px-4 xl:px-10">
                         {isMobile ? (
                             <Carousel autoPlay interval={3000} showThumbs={false} showStatus={false} infiniteLoop useKeyboardArrows>
                                 {['logo3', 'logo2', 'logo1', 'logo4', 'logo5', 'logo6'].map((tech, index) => (
                                     <div key={index} className="flex justify-center">
-                                        <Image src={`/images/tech/${tech}.png`} alt={tech} width={100} height={60} className="m-24" />
+                                        <Image src={`/images/tech/${tech}.svg`} alt={tech} width={60} height={30} className="m-8 w-32 h-32" />
                                     </div>
                                 ))}
                             </Carousel>
                         ) : (
-                            <div className="flex flex-wrap lg:gap-10 xl:gap-2 2xl:gap-10 justify-center mt-2">
+                            <div className="flex flex-wrap lg:gap-1 xl:gap-2 2xl:gap-10 justify-center mt-2 xl:mt-6">
                                 {['logo3', 'logo2', 'logo1', 'logo4', 'logo5', 'logo6'].map((tech, index) => (
-                                    <Image key={index} src={`/images/tech/${tech}.png`} alt={tech} width={100} height={40} className="m-5 lg:mx-5 xl:mx-12 md:w-20 md:h-20 sm:h-10" />
+                                    <Image key={index} src={`/images/tech/${tech}.png`} alt={tech} width={100} height={40} className="m-5 lg:mx-10 xl:mx-12 md:w-20 md:h-20 sm:h-10" />
                                 ))}
                             </div>
                         )}
