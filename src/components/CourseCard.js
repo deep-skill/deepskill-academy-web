24bca3a9bf8bb7a47d6c452d0e03928df264f7a4
Manuel Mandujano <manduuu2294@gmail.com>
2024-11-11 15:18:14 -0500

    change of the start date of MC Algorithms in the main landing page.

--- src/components/CourseCard.js
+++ src/components/CourseCard.js
@@ -14,7 +14,7 @@ const CourseCard = () => {
                 'Requisito nivel intermedio en programación.',
                 'Pruebas de código con estilo big techs.',
             ],
-            date: '04 Oct 2024',
+            date: '06 Enero 2025',
             imageSrc: '/images/image-course-card/image-course.png',
             link: '/algorithms-data',
             type: 'MASTERCLASS',
